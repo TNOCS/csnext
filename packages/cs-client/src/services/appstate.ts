cd46f9a66268f4072caa7b4aaabbb677b33a1cb8
damylen <damylen@gmail.com>
2017-10-31 14:52:19 +0100

    Added AppState documention + clean up

--- packages/cs-client/src/services/appstate.ts
+++ packages/cs-client/src/services/appstate.ts
@@ -1,53 +1,68 @@
-import { csdashboard } from './../components/csdashboard/csdashboard';
 import Vue from 'vue';
-// import { Single, IManagerConfig } from './../index';
-import * as cs from '@csnext/cs-core';
+import { csdashboard } from './../components/csdashboard/csdashboard';
+import { Project, IDataSourceHandler, Dashboard, IDataSource, AppTheme, FooterOptions, NavigationOptions } from '@csnext/cs-core';
 import { Single, Grid, Logger, IManagerConfig, WebRequestDataSourceHandler } from '../index';
 
+/** AppState is a singleton class used for project defintion, keeping track of available dashboard managers and datasource handlers. It also includes a generic EventBus and logger instance */
 export class AppState {
 
-    public project: cs.Project = {};
+    /** Project definition */
+    public project: Project = {};
+
+    /** Available dashboard managers for layouting */
     public dashboardManagers: { [id: string]: IManagerConfig } = {};
-    public dataSourceHandlers: { [id: string]: cs.IDataSourceHandler } = {};
-    public data: { [id: string] : any} = {};
+
+    /** Available datasource handlers  */
+    public dataSourceHandlers: { [id: string]: IDataSourceHandler } = {};
+
+    /** Logger */
     public L = Logger.Instance;
+
+    /** Event bus for publish/subscribe events in application */
     public EventBus = new Vue();
-    public sideBarComponents: any[] = [];
-    private static _instance: AppState;
+
+    /** True if the application has been initialized */
     public isInitialized = false;
 
-    constructor() {
-    }
+    /** used for singleton  */
+    private static _instance: AppState;
 
-    public static get Instance() {
-        // Do you need arguments? Make it a regular method instead.
+    /** Get singleton instance of appstate */
+    public static get Instance() {        
         return this._instance || (this._instance = new this());
     }
 
+    /** Initialize the project state, dashboard managers and data source handlers */
     public Init() {
         this.L.info('appstate', 'Init AppState');
-        this.AddManager(<IManagerConfig>{ id: 'single', name: 'single page', component: Single });
-        this.AddManager(<IManagerConfig>{ id: 'grid', name: 'grid page', component: Grid });
+        this.AddDashboardManager(<IManagerConfig>{ id: 'single', name: 'single page', component: Single });
+        this.AddDashboardManager(<IManagerConfig>{ id: 'grid', name: 'grid page', component: Grid });
         this.AddDataSourceHandler(new WebRequestDataSourceHandler());
+
+        if (!this.project) { this.project = new Project();};
+        if (!this.project.theme) { this.project.theme = new AppTheme(); }
+        if (!this.project.footer) { this.project.footer = new FooterOptions(); }
+        if (!this.project.dashboards) { this.project.dashboards = []; }
+        if (!this.project.navigation) { this.project.navigation = new NavigationOptions();}
+
         this.isInitialized = true;
         this.EventBus.$emit('init');
     }
 
-    public AddManager(manager: IManagerConfig) {
+    /** Registration of a new dashboard manager */
+    public AddDashboardManager(manager: IManagerConfig) {
         this.dashboardManagers[manager.id] = manager;
     }
 
-    public AddDataSourceHandler(handler: cs.IDataSourceHandler) {
+    /** Registration of a new data source handler */
+    public AddDataSourceHandler(handler: IDataSourceHandler) {
         this.dataSourceHandlers[handler.id] = handler;
     }
 
-    public AddDashboard(elementId: string, dashboard: cs.Dashboard) {
-        let dash = csdashboard;
-    }
-
-    public LoadDataSource(source: cs.IDataSource | string): Promise<any> {
+    /** Load a data source using the assigned data source handler */
+    public LoadDataSource(source: IDataSource | string): Promise<any> {
         return new Promise((resolve, reject) => {
-            let src: cs.IDataSource;
+            let src: IDataSource;
             if (typeof (source) === 'string') {
                 if (this.project.dataSources && this.project.dataSources.hasOwnProperty(source)) {
                     src = this.project.dataSources[source];
@@ -67,6 +82,8 @@ export class AppState {
             }
         });
     }
+
+    /** load all data sources */
     public LoadDataSources() {
         if (this.project.dataSources) {
             for (let ds in this.project.dataSources) {
