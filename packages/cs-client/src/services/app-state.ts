cfff3b1090382e0a7d6b0d6d5c03bac78987fcb7
damylen <damylen@gmail.com>
2018-04-26 11:01:39 +0200

    cs-client: added helper method for adding rightsidebar widget

--- packages/cs-client/src/services/app-state.ts
+++ packages/cs-client/src/services/app-state.ts
@@ -1,9 +1,17 @@
-import Vue from 'vue';
-import { MessageBusService, Project, IProject, INotification, IDashboard, IDatasource } from '@csnext/cs-core';
-import { CsApp } from './../components/cs-app/cs-app';
-import { ProjectManager } from './project-manager';
-import { CsSettings, CsDashboard, Logger, CsWidget, guidGenerator } from '../';
-import VueRouter from 'vue-router';
+import { IWidget } from "./../../../cs-core/dist/classes/widget.d";
+import Vue from "vue";
+import {
+  MessageBusService,
+  Project,
+  IProject,
+  INotification,
+  IDashboard,
+  IDatasource
+} from "@csnext/cs-core";
+import { CsApp } from "./../components/cs-app/cs-app";
+import { ProjectManager } from "./project-manager";
+import { CsSettings, CsDashboard, Logger, CsWidget, guidGenerator } from "../";
+import VueRouter from "vue-router";
 
 /** AppState is a singleton class used for project defintion, keeping track of available dashboard managers and datasource handlers. It also includes a generic EventBus and logger instance */
 // TODO Should we use idiomatic Typescript instead, as in
@@ -43,20 +51,22 @@ export class AppState {
 
   public windowSize = { x: 0, y: 0 };
 
-  private constructor() { }
+  private constructor() {}
 
   /** Initialize the project state, dashboard managers and data summaries handlers */
   public init(project: Project) {
-    Logger.info('app-state', 'Init AppState');
+    Logger.info("app-state", "Init AppState");
 
-    Vue.component('cs-dashboard', CsDashboard);
-    Vue.component('cs-widget', CsWidget);
-    Vue.component('cs-app', CsApp);
+    Vue.component("cs-dashboard", CsDashboard);
+    Vue.component("cs-widget", CsWidget);
+    Vue.component("cs-app", CsApp);
 
     this.project = project;
 
     // make sure all dashboards are marked as main
-    if (this.project.dashboards) { this.initializeDashboards(this.project.dashboards); }
+    if (this.project.dashboards) {
+      this.initializeDashboards(this.project.dashboards);
+    }
 
     this.projectManager = new ProjectManager(project);
 
@@ -70,26 +80,58 @@ export class AppState {
     // }
 
     this.isInitialized = true;
-    this.bus.publish('app-state', 'init', null);
+    this.bus.publish("app-state", "init", null);
   }
 
   public initializeDashboards(dashboards: IDashboard[]) {
     if (dashboards) {
       dashboards.forEach(d => {
         d.isMain = true;
-        if (d.dashboards) { this.initializeDashboards(d.dashboards); }
+        if (d.dashboards) {
+          this.initializeDashboards(d.dashboards);
+        }
       });
     }
   }
 
   public loadDatasource(source: IDatasource | string): Promise<object> {
-    if (!this.projectManager) { return Promise.reject('Project Manager not initialized'); }
+    if (!this.projectManager) {
+      return Promise.reject("Project Manager not initialized");
+    }
     return this.projectManager.datasourceManager.load(source);
   }
 
   public TriggerNotification(notification: INotification) {
-    Object.assign(notification, { id: guidGenerator(), timeout: 3000, created: new Date(), isRead: false, remember: true, _visible: true } as INotification);
-    this.bus.publish('notification', 'new', notification);
-    if (this.project.notifications && this.project.notifications.items && notification.remember) { this.project.notifications.items.push(notification); }
+    Object.assign(notification, {
+      id: guidGenerator(),
+      timeout: 3000,
+      created: new Date(),
+      isRead: false,
+      remember: true,
+      _visible: true
+    } as INotification);
+    this.bus.publish("notification", "new", notification);
+    if (
+      this.project.notifications &&
+      this.project.notifications.items &&
+      notification.remember
+    ) {
+      this.project.notifications.items.push(notification);
+    }
+  }
+
+  /** If a rightsidebar exists, it will replaces all rightsidebar content with this specific widget */
+  public OpenRightSidebarWidget(widget: IWidget) {
+    if (
+      this.project.rightSidebar &&
+      this.project.rightSidebar.dashboard &&
+      this.project.rightSidebar.dashboard.widgets
+    ) {
+      while (this.project.rightSidebar.dashboard.widgets.length > 0) {
+        this.project.rightSidebar.dashboard.widgets.pop();
+      }
+      this.project.rightSidebar.dashboard.widgets.push(widget);
+      this.project.rightSidebar.open = true;
+    }
   }
 }
--- packages/cs-core/src/classes/theme-colors.ts
+++ packages/cs-core/src/classes/theme-colors.ts
@@ -1,9 +1,10 @@
 export class ThemeColors {
-  public primary = '#1976D2';
-  public secondary = '#424242';
-  public accent = '#82B1FF';
-  public error = '#FF5252';
-  public info = '#2196F3';
-  public success = '#4CAF50';
-  public warning = '#FFC107';
+  public primary: string = '#1976D2';
+  public secondary: string = '#424242';
+  public accent: string = '#82B1FF';
+  public error: string = '#FF5252';
+  public info: string = '#2196F3';
+  public success: string = '#4CAF50';
+  public warning: string = '#FFC107';
+  public menu: string = '#000';
 }
