f93f0be34ff52303711c817cd97315c75e85837b
damylen <damylen@gmail.com>
2018-05-18 15:18:57 +0200

    docs: small updates

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "325265d6d1ee3fdadf371dfdd667f91b"
+    "revision": "be09378830491abd236cc7c2a9f87ce5"
   },
   {
     "url": "assets/css/3.styles.3710225f.css",
