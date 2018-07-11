8642a74f5987b2bccbfdfa17198964c0688d5dae
damylen <damylen@gmail.com>
2018-07-11 22:57:25 +0200

    cs-client: first mobile navigation

--- packages/cs-core/src/classes/navigation-options.ts
+++ packages/cs-core/src/classes/navigation-options.ts
@@ -2,7 +2,7 @@ import { ISearchOptions } from './search-options';
 
 export class NavigationOptions {
   public style?: 'left' | 'left-hierarchy' | 'tabs' | 'bottom';
-  public hideTitle?: boolean; 
+  public hideTitle?: boolean;
   public hideMenuToggle?: boolean;
   public floating?: boolean;
   public search?: ISearchOptions;
