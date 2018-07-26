a2b268394744f4a6b7ca46f38ec1e11deed165a7
damylen <damylen@gmail.com>
2018-07-26 11:48:50 +0200

    embed md fonts for offline support, removed vue-tour dependency

--- packages/cs-client/src/widgets/intro/intro-widget.ts
+++ packages/cs-client/src/widgets/intro/intro-widget.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import {
-  IWidget} from '@csnext/cs-core';
+import { IWidget } from '@csnext/cs-core';
 import './intro-widget.css';
 import { AppState } from '../..';
 import { IIntroWidgetOptions } from './intro-widget-options';
@@ -19,13 +18,13 @@ export class CsIntroWidget extends Vue {
   public app = AppState.Instance;
 
   public get options(): IIntroWidgetOptions | undefined {
-    if (!this.widget || !this.widget.options) { return undefined; }
+    if (!this.widget || !this.widget.options) {
+      return undefined;
+    }
     return this.widget.options as IIntroWidgetOptions;
   }
 
   constructor() {
     super();
   }
-
- 
 }
--- packages/cs-client/webpack.config.js
+++ packages/cs-client/webpack.config.js
@@ -3,29 +3,12 @@ const path = require('path');
 const env = require('yargs').argv.env; // use --env with webpack 2
 // const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
-
 let libraryName = 'csclient';
 
 let plugins = [],
   outputFile;
 
 if (env === 'build') {
-  // const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-  // plugins.push(
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     comments: false,
-  //     compress: {
-  //       unused: true,
-  //       dead_code: true,
-  //       warnings: false,
-  //       drop_debugger: true,
-  //       conditionals: true,
-  //       evaluate: true,
-  //       drop_console: true,
-  //       sequences: true,
-  //       booleans: true,
-  //     }
-  //   }));
   outputFile = libraryName + '.[name].min.js';
 } else {
   outputFile = libraryName + '.[name].js';
@@ -40,54 +23,72 @@ const output = {
 };
 
 const mod = {
-  rules: [{
-    test: /\.ts$/,
-    exclude: /node_modules/,
-    enforce: 'pre',
-    loader: 'tslint-loader'
-  }, {
-    test: /\.ts$/,
-    exclude: /node_modules/,
-    loader: 'ts-loader'
-  }, {
-    test: /\.html$/,
-    loader: 'raw-loader',
-    exclude: ['./src/index.html']
-  }, {
-    test: /\.scss$/,
-    use: [{
-      loader: 'style-loader'
-    }, {
-      loader: 'css-loader'
-    }, {
-      loader: 'sass-loader'
-    }]
-  }, {
-    test: /\.css$/,
-    use: [{
-      loader: 'style-loader'
-    }, {
-      loader: 'css-loader'
-    }]
-  }]
+  rules: [
+    {
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      enforce: 'pre',
+      loader: 'tslint-loader'
+    },
+    {
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      loader: 'ts-loader'
+    },
+    {
+      test: /\.html$/,
+      loader: 'raw-loader',
+      exclude: ['./src/index.html']
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'style-loader'
+        },
+        {
+          loader: 'css-loader'
+        },
+        {
+          loader: 'sass-loader'
+        }
+      ]
+    },
+    { test: /\.woff2$/, 
+      use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'style-loader'
+        },
+        {
+          loader: 'css-loader'
+        }
+      ]
+    }
+  ]
 };
 
 function buildConfig(entry, externals, analyzer) {
   let pl = []; //new HardSourceWebpackPlugin()
-  pl.push(new webpack.WatchIgnorePlugin([
-    /\.js$/,
-    /\.d\.ts$/
-  ]));
+  pl.push(new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]));
+  
   if (analyzer) {
-    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-    pl.push(new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-      reportFilename: 'reports/report.' + analyzer + '.html',
-      generateStatsFile: false
-    }));
+    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+      .BundleAnalyzerPlugin;
+    pl.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: 'reports/report.' + analyzer + '.html',
+        generateStatsFile: false
+      })
+    );
   }
-  return baseConfig = {
+  
+  return (baseConfig = {
     entry: entry,
     // entry: __dirname + '/src/index.ts',
     devtool: 'source-map',
@@ -98,17 +99,21 @@ function buildConfig(entry, externals, analyzer) {
       extensions: ['.ts', '.js', '.html']
     },
     plugins: plugins.concat(pl)
-  };
+  });
 }
 
 const config = [
-  buildConfig({
-    cs: ["./src/index.ts"]
-  }, {
-      'vue': 'Vue',
-      'vuetify': 'vuetify',
-      'vue-markdown': 'vue-markdown'      
-    }, 'cs')
+  buildConfig(
+    {
+      cs: ['./src/index.ts']
+    },
+    {
+      vue: 'Vue',
+      vuetify: 'vuetify',
+      'vue-markdown': 'vue-markdown'
+    },
+    'cs'
+  )
   // , buildConfig({ vuebundle: ["vue", "vue-router"] }, 'csvue')
 ];
 
