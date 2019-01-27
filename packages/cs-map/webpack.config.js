364046d9acf9b9e62ce17b45c66266a54aec71c6
damylen <damylen@gmail.com>
2019-01-27 14:29:38 +0100

    all: upgrade packages

--- packages/cs-map/webpack.config.js
+++ packages/cs-map/webpack.config.js
@@ -5,17 +5,19 @@ const config = buildConfig(
     'csmap',
     { cs: ['./src/index.ts'] },
     {
-        '@csnext/cs-core': '@csnext/cs-core',   
-        'cs-core':'cs-core',
+        '@csnext/cs-core': '@csnext/cs-core',
+        'cs-core': 'cs-core',
         '@csnext/cs-client': '@csnext/cs-client',
         'cs-client': 'cs-client',
         vue: 'Vue',
         Vue: 'Vue',
         'mapbox-gl': 'mapbox-gl',
-        'axios': 'axios',
+        axios: 'axios',
         '@mapbox/mapbox-gl-draw': '@mapbox/mapbox-gl-draw',
         '@mapbox/mapbox-gl-draw/dist': '@mapbox/mapbox-gl-draw/dist',
-        'vue-perfect-scrollbar': 'vue-perfect-scrollbar'
+        'vue-perfect-scrollbar': 'vue-perfect-scrollbar',
+        'handlebars/runtime': 'handlebars/dist/cjs/handlebars.runtime',
+        handlebars: 'handlebars/dist/cjs/handlebars.runtime'
     },
     'csmap'
 );
