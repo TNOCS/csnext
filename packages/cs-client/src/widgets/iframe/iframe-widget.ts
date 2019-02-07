0992a949392c94952e22e7b81872c91e4e0b0764
damylen <damylen@gmail.com>
2019-02-07 17:10:57 +0100

    cs-client: refactoring iframe files, default scrolling=no

---
+++ packages/cs-client/src/widgets/iframe/iframe-widget.ts
@@ -0,0 +1,17 @@
+import Component from 'vue-class-component';
+import { WidgetBase } from '../..';
+import { IWidget } from '@csnext/cs-core';
+
+@Component({
+  name: 'iframe-widget',
+  template:
+    '<iframe :src="widget.data.url" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>',
+  props: {
+    widget: null
+  }
+} as any)
+export class IframeWidget extends WidgetBase {
+
+     /** access the original widget from configuration */
+  public widget!: IWidget;
+}
