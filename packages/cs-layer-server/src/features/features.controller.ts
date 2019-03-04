8b532caeb2b74f79ad1df1ee80924a7dd63ee966
damylen <damylen@gmail.com>
2019-03-04 09:56:03 +0100

    cs-layer-server: added delete feature api

--- packages/cs-layer-server/src/features/features.controller.ts
+++ packages/cs-layer-server/src/features/features.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Controller, Param, Post, Body, Query, Put } from '@nestjs/common';
+import { Get, Controller, Param, Post, Body, Query, Put, Delete } from '@nestjs/common';
 import { LayerService } from '../layers/layers.service';
 import {
   ApiUseTags,
@@ -39,5 +39,30 @@ export class FeatureController {
   @Post(':sourceid/:featureid')
   postFeature(@Body() body: Feature, @Param('sourceid') sourceid: string, @Param('featureid') featureid?: string): Promise<Feature | undefined> {    
       return this.layerService.updateFeature(sourceid, body, featureid);
+  }
+
+  @ApiOperation({
+    title: 'Delete feature',
+    description: 'Delete feature'
+  })
+  @ApiImplicitParam({
+    name: 'sourceid',
+    required: true,
+    description: 'Specify the layer/source id for the layer you want to update'
+  })
+  @ApiImplicitParam({
+    name: 'featureid',
+    required: true,
+    description: 'Specify the feature id for the layer you want to update'
+  })
+ 
+  @ApiResponse({
+    status: 200,
+    description: 'Returns true if succeeded',
+    type: Boolean
+  })
+  @Delete(':sourceid/:featureid')
+  deleteFeature(@Param('sourceid') sourceid: string, @Param('featureid') featureid: string): Promise<boolean> {    
+      return this.layerService.deleteFeature(sourceid, featureid);
   }
 }
