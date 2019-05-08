e2e5e61237938378ab43265b6917b91040a3fe45
damylen <damylen@gmail.com>
2019-05-08 10:28:48 +0200

    cs-client: added cs-loader

---
+++ packages/cs-client/src/components/cs-loader/cs-loader.ts
@@ -0,0 +1,18 @@
+import Component from 'vue-class-component';
+import Vue from 'vue';
+import { pleaseWait } from 'please-wait';
+import 'please-wait/build/please-wait.css';
+import { Watch } from 'vue-property-decorator';
+
+@Component({
+  name: 'cs-loader',
+  props: ['isLoading'],
+  template: require('./cs-loader.html')
+} as any)
+export class CsLoading extends Vue {
+
+  @Watch('isLoading')
+  public loadingChanged(d: any) {
+    console.log('Loading changed 2');
+  }
+}
