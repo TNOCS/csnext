8d3704d9a60b27a8b6e5930b228caf08fb1f188b
arnoud <damylen@gmail.com>
2019-09-23 16:50:24 +0200

    cs-markdown/cs-client, moved markdown support to seperate package

--- packages/cs-client/src/widgets/markdown/md-widget.ts
+++ packages/cs-client/src/widgets/markdown/md-widget.ts
@@ -1,29 +1,14 @@
 import Component from 'vue-class-component';
-import VueMarkdown from 'vue-markdown';
-import Vue from 'vue';
-import { IWidget } from '@csnext/cs-core';
-import { Watch } from 'vue-property-decorator';
-import { AppState } from '../../services/app-state';
+import { WidgetBase } from '../widget-base';
 
 @Component({
   name: 'md-widget',
   props: {
     widget: null
   },
-  components: { VueMarkdown },
   template:
-    '<VueMarkdown v-if="widget" :source="markdown" :emoji="widget.options.emoji" :linkify="widget.options.linkify" :breaks="widget.options.break" :html="widget.options.html"></VueMarkdown>'
+    '<span>{{widget.data}}</span>'
 } as any)
-export class MdWidget extends Vue {
+export class MdWidget extends WidgetBase {
   /** access the original widget from configuration */
-  public widget!: IWidget;
-
-  public get markdown(): any {
-    if (this.widget && this.widget.datasource) {
-      return this.widget.content;
-    } else if (this.widget && this.widget.data) {
-      return this.widget.data;
-    }
-    return '';
-  }
 }
