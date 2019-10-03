232d3bcecbc465ab96f46fb744692deabbd5324f
arnoud <damylen@gmail.com>
2019-10-03 13:30:04 +0200

    cs-core: updated dashboard/widget form definitions

--- packages/cs-core/src/dashboard/toolbar-options.ts
+++ packages/cs-core/src/dashboard/toolbar-options.ts
@@ -1,9 +1,15 @@
+import { FormField, Form } from '../form/form-decorators';
 export class ToolbarOptions {
     public hide?: boolean;
+    @FormField({ title: 'Flat', type: 'checkbox' })
     public flat?: boolean;
+    @FormField({ title: 'Dense', type: 'checkbox' })
     public dense?: boolean;
+    @FormField({ title: 'Hide Icon', type: 'checkbox' })
     public hideIcon?: boolean;
+    @FormField({ title: 'Hide Title', type: 'checkbox' })
     public hideTitle?: boolean;
+    @FormField({ title: 'Elevation', type: 'number', min: 0, max: 11 })
     public elevation?: number;
 }
 
