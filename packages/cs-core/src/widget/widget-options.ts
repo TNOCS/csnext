11e020e319d5121adbaeedf89a7083eeb68c21a3
arnoud <damylen@gmail.com>
2019-10-03 16:40:11 +0200

    cs-core: updated widget-options form definitions

--- packages/cs-core/src/widget/widget-options.ts
+++ packages/cs-core/src/widget/widget-options.ts
@@ -1,11 +1,14 @@
 import { IMenu, ToolbarOptions } from './../';
+import { FormField, Form } from '../form/form-decorators';
 
+@Form({ title: 'Widget Options' })
 export class WidgetOptions {
   public datasource?: string;
   public class?: string;
   public width?: number;
   public height?: number;
   public margin?: number;
+  @FormField({ title: 'Background Color', type: 'string' })
   public backgroundColor?: string;
   public x?: number;
   public y?: number;
@@ -15,11 +18,19 @@ export class WidgetOptions {
   public canEdit?: boolean;
   public menus?: IMenu[];
   public widgetBorder?: string;
+  @FormField({ title: 'Hide menu', type: 'checkbox' })
+  public hideMenu?: boolean;
   public hide?: boolean;
+  @FormField({ title: 'Show toolbar', type: 'checkbox' })
   public showToolbar?: boolean;
+  // @FormField({title: 'Toolbar options', type: 'object'})
   public toolbarOptions?: ToolbarOptions;
+  @FormField({ title: 'Title', type: 'string' })
   public title?: string;
+  @FormField({ title: 'Elevation', type: 'slider', min: 0, max: 10 })
   public elevation?: number;
+  @FormField({ title: 'Flat', type: 'checkbox' })
   public flat?: boolean;
+  @FormField({ title: 'Outlined', type: 'checkbox' })
   public outlined?: boolean;
 }
