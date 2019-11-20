5ea414706e25ce3c2324149c940b30c46d10eecd
rinze.bruining <rwbpost@gmail.com>
2019-11-20 10:24:50 +0100

    cs-markdown: option to use data field for content

--- packages/cs-markdown/src/cs-markdown/cs-markdown.ts
+++ packages/cs-markdown/src/cs-markdown/cs-markdown.ts
@@ -21,37 +21,49 @@ export class CsMarkdown extends WidgetBase {
 
     public content: string = "";
     // public options: MdWidgetOptions = {};
-    @Prop({ default: '' }) private data?: string;
+    @Prop({ default: undefined }) private data?: string | {url: string};
 
     @Watch('widget.data', { deep: true })
+    public widgetDataUpdated() {
+        this.updateContent();
+    }
+
+    @Watch('data', { deep: true })
     public dataUpdated() {
         this.updateContent();
     }
 
     private updateContent() {
-        if (!this.widget) return;
-        if (typeof (this.widget.data) === 'string') {
-            Vue.set(this, 'content', this.widget.data);
-        } else if (typeof (this.widget.data) === 'string') {
-            Vue.set(this, 'content', this.widget.data);
+        if (!this.widget && !this.data) return;
+        if (this.data) {
+            this.setContent(this.data);
+        } else if (this.widget) {
+            this.setContent(this.widget.data);
+        }
+    }
+
+    private setContent(data: string | {url: string}) {
+        if (typeof (data) === 'string') {
+            Vue.set(this, 'content', data);
+        } else if (typeof (data) === 'string') {
+            Vue.set(this, 'content', data);
         } else {
-            if (this.widget.data && this.widget.data.hasOwnProperty('url')) {
-                axios.get(this.widget.data.url).then(u => {
+            if (data && data.hasOwnProperty('url') && data.url) {
+                axios.get(data.url).then(u => {
                     if (u && u.data) {
                         Vue.set(this, 'content', u.data);
                     }
                 })
-
             }
         }
     }
 
-    @Watch('widget.content', { deep: true })
-    public contentUpdated() {
-        // if (this.chart) {
-        //     this.chart.load(this.widget.content);
-        // }
-    }
+    // @Watch('widget.content', { deep: true })
+    // public contentUpdated() {
+    //     // if (this.chart) {
+    //     //     this.chart.load(this.widget.content);
+    //     // }
+    // }
 
     public mounted() {
         this.updateContent();
