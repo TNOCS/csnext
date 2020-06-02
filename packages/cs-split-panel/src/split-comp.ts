570aa425f67b6dfac97c26806dfcb3285e7e6117
damylen <damylen@gmail.com>
2020-06-03 01:12:42 +0200

    Feature/cs splitpanel build (#52)
    * cs-split-panel: try to fix nesting issue
    
    * cs-split-panel: removed recursion for now
    
    * github: remove feature branches from main build action
    
    Co-authored-by: Rinze <rinze.bruining@tno.nl>

--- packages/cs-split-panel/src/split-comp.ts
+++ packages/cs-split-panel/src/split-comp.ts
@@ -1,22 +1,34 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
 import { IDashboard, IWidget } from '@csnext/cs-core';
 import './split-panel.css';
 import { SplitGrid, SplitGridArea, SplitGridGutter } from 'vue-split-grid';
+import { SplitElement } from './split-element';
 
 @Component({
     template: require('./split-comp.html'),
     components: {
+        SplitComp,
         SplitGrid,
         SplitGridArea,
         SplitGridGutter
-    },
-    props: {
-        options: null,
-        dashboard: null
     }
 } as any)
 export class SplitComp extends Vue {
+
+    @Prop({ default: null }) private options;
+    @Prop({ default: null }) private dashboard;
+
+    constructor() {
+        super();
+        console.log(this.options);
+    }
+
+    public getSplitpanel(panelElm: SplitElement) {
+        console.log(panelElm);
+        return panelElm && panelElm.splitpanel ? panelElm.splitpanel : undefined;
+    }
 
     public getDirection(dir: string) {
         switch (dir) {
@@ -26,7 +38,7 @@ export class SplitComp extends Vue {
                 return 'column';
             default:
                 return dir;
-        }        
+        }
     }
 
     public getWidget(id: string, dashboard: IDashboard): IWidget | undefined {
