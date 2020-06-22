988163c3a0b3c0f158252ac210608bc68b40de55
Rinze <rinze.bruining@tno.nl>
2020-06-22 10:21:08 +0200

    cs-split-panel: fix interface definition for sizeUnit (is optional)

--- packages/cs-split-panel/src/split-element.ts
+++ packages/cs-split-panel/src/split-element.ts
@@ -2,7 +2,7 @@ import { SplitPanelOptions } from './.';
 
 export class SplitElement {
     public size?: number;
-    public sizeUnit: string = '%';
+    public sizeUnit?: string = '%';
     public minSize?: number;
     public widgetId?: string;
     public splitpanel?: SplitPanelOptions;
