5a171a35233c7dede142d5a9a317a36046c6583f
arnoud <damylen@gmail.com>
2020-10-19 09:15:44 +0200

    layer-server: updated LayerStyle definition

--- packages/cs-layer-server/src/shared/layer-style.ts
+++ packages/cs-layer-server/src/shared/layer-style.ts
@@ -1,7 +1,21 @@
+import { CirclePaint } from 'mapbox-gl';
 import { MapboxStyles, ILayerStyle } from './mapbox-styles';
+
+export type ClusterSettings = {
+    cluster?: number | boolean;
+
+    clusterRadius?: number;
+
+    clusterMaxZoom?: number;
+
+    clusterProperties?: object;
+
+    paint?: CirclePaint;
+}
 
 export class LayerStyle implements ILayerStyle {
     public types?: string[];
+    public type?: 'fill' | 'line' | 'symbol' | 'circle' | 'fill-extrusion' | 'raster' | 'background' | 'heatmap' | 'hillshade';
     public iconZoomLevel?: number;
     public title?: string;
     public mapTitle?: string;
@@ -9,6 +23,10 @@ export class LayerStyle implements ILayerStyle {
     public fill?: boolean;
     public line?: boolean;
     public pointCircle?: boolean;
+    public showSymbol?: boolean;
+    public hoverOpacity?: number = 75;
+    public opacity?: number;
+    public clusterSettings?: ClusterSettings;
     public icon?: string;
     public icons?: { [id: string]: string };
     public mapbox?: MapboxStyles;
