855d07b3e0f443322972e80220f03bd89cd73127
arnoud <damylen@gmail.com>
2020-12-04 17:36:17 +0100

    cs-client: removed test files

--- packages/cs-client/src/components/test/test.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Component from 'vue-class-component';
-
-// import './home.scss';
-@Component({
-  template: require('./test.html')
-})
-export class Test extends Vue {
-}
