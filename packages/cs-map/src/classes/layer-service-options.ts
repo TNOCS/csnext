c087d6de1adc3ee0a7b847d2da2222ad3ad0c586
Rinze <rwbpost@outlook.com>
2020-12-15 18:08:52 +0100

    server: amend prev commit

--- packages/cs-map/src/classes/layer-service-options.ts
+++ packages/cs-map/src/classes/layer-service-options.ts
@@ -1,9 +1,11 @@
 export interface ILayerServiceOptions {
     tags?: string[];
     openFeatureDetails?: boolean;
+    setSourceUrl?: boolean;
 }
 
 export abstract class LayerServiceOptions implements ILayerServiceOptions{
     public tags?: string[];
     public openFeatureDetails?: boolean;
+    public setSourceUrl?: boolean;
 }
