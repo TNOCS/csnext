e73d62fcd786179af88d41ba880b1b11cb0781c8
arnoud <damylen@gmail.com>
2021-01-11 14:12:49 +0100

    cs-data: update property-type form definition

--- packages/cs-data/src/classes/property-type.ts
+++ packages/cs-data/src/classes/property-type.ts
@@ -2,6 +2,8 @@ import { Form, FormField } from '@csnext/cs-core';
 import { LayerLegend } from './layer-legend';
 import { RelationType } from './relation-type';
 
+
+
 @Form({
     title: 'Property',
     hideTitle: true,
@@ -15,38 +17,46 @@ import { RelationType } from './relation-type';
 })
 /** Property description */
 export class PropertyType {
+
+    static isNumber = [ (v: any) => { return v.type && v.type === 'number'}];
+    static isString = [ (v: any) => { return v.type && v.type === 'string'}];
+    static isUrl = [ (v: any) => { return v.type && v.type === 'url'}];
     
     @FormField({ title: 'Label', type: 'string' })
     public label?: string;
-    @FormField({ title: 'Label', type: 'string' })
+    @FormField({ title: 'Description', type: 'string', optional: true })
     public description?: string;
     public count?: number;
-    @FormField({ title: 'Default', type: 'number', group: 'number' })
+    @FormField({ title: 'Default', type: 'number', group: 'number', optional: true })
     public default?: any;
-    
+    @FormField({ title: 'Unit', type: 'string', requirements: PropertyType.isNumber })
     public unit?: string;
+    @FormField({ title: 'Decimals', type: 'number', requirements: PropertyType.isNumber })
     public decimals?: number;
-    @FormField({ title: 'Handlebar format', type: 'string' })
+    @FormField({ title: 'Handlebar format', type: 'string', optional: true, requirements: PropertyType.isString })
     public handlebarFormat?: string;
     
-    @FormField({ title: 'Max', type: 'number', group: 'number' })
+    @FormField({ title: 'Max', type: 'number', group: 'number', requirements: PropertyType.isNumber, optional: true})
     public max?: number;
     // public options?: any[];
     public mean?: number;
     public bins?: number[];
     public median?: number;
-    @FormField({ title: 'Min', type: 'number', group: 'number' })
+    @FormField({ title: 'Min', type: 'number', group: 'number', requirements: PropertyType.isNumber, optional: true })
     public min?: number;
     public options?: { [key: string]: string };
-    public sd?: number;
-    @FormField({ title: 'Section', type: 'string' })
+    public sd?: number;    
     public section?: string;
-    @FormField({ title: 'String format', type: 'string' })
-    public stringFormat?: string;
-    @FormField({ title: 'Title', type: 'string' })
+    @FormField({ title: 'String format', type: 'string', requirements: PropertyType.isString, optional: true })
+    public stringFormat?: string;    
     public title?: string;
     public colorScheme?: string | string[];    
+    @FormField({ title: 'Type', 
+        required: true,
+        defaultValue: 'text',
+        type: 'selection', options: ['string', 'number', 'url', 'relation'] })
     public type?: string;
+
     public unique?: number;
     public legendStyle?: any;
     public resource?: string;
@@ -54,17 +64,12 @@ export class PropertyType {
     public relation?: RelationType;
     public hidden?: boolean;
     public readonly?: boolean;  
+    @FormField({ title: 'Url template', type: 'string', requirements: PropertyType.isUrl, optional: true })
     public urlTemplate?: string;  
 
     public class?: string;
     public _initialized?: boolean = false;
-    @FormField({
-        title: 'Type',
-        type: 'selection',
-        defaultValue: 'text',
-        required: true,
-        options: ['text', 'number']
-    })
+    public _originalType?: string;        
     public _key?: string;
     public _values?: any[];    
 }
