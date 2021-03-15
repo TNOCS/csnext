8c892149d8f6fb8036aa8f99cc4ccee552602bc1
arnoud <damylen@gmail.com>
2021-03-15 09:28:38 +0100

    cs-client: added helper method to find dashboard using id

--- packages/cs-client/src/services/dashboard-manager-base.ts
+++ packages/cs-client/src/services/dashboard-manager-base.ts
@@ -1,4 +1,4 @@
-import { IMessageBusService, IMessageBusCallback, MessageBusManager, IDashboardManager, IDashboard } from '@csnext/cs-core';
+import { IMessageBusService, IMessageBusCallback, MessageBusManager, IDashboardManager, IDashboard, IWidget } from '@csnext/cs-core';
 import { Logger } from '../';
 
 export class DashboardManagerBase implements IDashboardManager {
@@ -27,5 +27,37 @@ export class DashboardManagerBase implements IDashboardManager {
         if (this.busManager) {
             this.busManager.stop();
         }
+    }
+
+    private findDashboardInWidget(id: string, widget: IWidget) : IDashboard | undefined {
+        if (widget.dashboard) {
+            const res = this.findDashboard(id, widget.dashboard);
+            if (res) { return res;}
+        }
+        if (widget.widgets) {
+            for (const sw of widget.widgets) {
+                const res = this.findDashboardInWidget(id, sw);
+                if (res) { return res;}
+            }
+        }
+    }
+
+    public findDashboard(id: string, dashboard?: IDashboard) : IDashboard | undefined {
+        if (!dashboard) {
+            dashboard = this.dashboard;
+        }
+        if (dashboard.id === id) { return dashboard;}
+        if (dashboard.dashboards) {
+            for (const d of dashboard.dashboards) {
+                const res = this.findDashboard(id, d);
+                if (res) { return res;}        
+            }            
+        }
+        if (dashboard.widgets) {
+            for (const w of dashboard.widgets) {
+                const res = this.findDashboardInWidget(id, w);
+                if (res) { return res;}                
+            }
+        }    
     }
 }
