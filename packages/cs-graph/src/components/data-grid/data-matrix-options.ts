1a3cf91c4f51e05b41936106c7096c9fdbb04bcf
damylen <damylen@gmail.com>
2022-02-09 19:46:03 +0100

    ✨ cs-graph: extended datamatrix options

--- packages/cs-graph/src/components/data-grid/data-matrix-options.ts
+++ packages/cs-graph/src/components/data-grid/data-matrix-options.ts
@@ -2,13 +2,15 @@ import { WidgetOptions } from '@csnext/cs-core';
 import { GraphFilter } from '@csnext/cs-data';
 
 export class DataMatrixOptions extends WidgetOptions {
+    public matrixStyle?: 'relation';
     public horizontalType?: string;    
     public horizontalFilter?: GraphFilter;    
     public horizontalParentProperty?: string;
+    public horizontalValueProperty?: string;
     public verticalType?: string;    
     public verticalFilter?: GraphFilter;    
     public verticalParentProperty?: string;
+    public verticalValueProperty?: string;
     public valueType?: string;
     public valueProperty?: string;
-    
 }
